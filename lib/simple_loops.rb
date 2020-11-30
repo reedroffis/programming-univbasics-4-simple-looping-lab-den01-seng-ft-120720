12349e500ed0c0d997e4a013acae6047123238f9
Reed Roffis <reedroffis18@gmail.com>
2020-11-30 22:25:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,10 +23,11 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  new_array = [] 
   count = 0 
+  new_array = [] 
   while count < array.length do 
     new_array.push(array[count].to_s)
+    count += 1 
   end 
   new_array
 end
